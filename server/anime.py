d24630bf9b4b5da194067494b110a83da3ef96e7
Guilherme Simao Barbosa da Silva <guilhermesimaogh@outlook.com>
2024-10-20 01:34:16 -0300

    Adicionado novo endpoint para numero limite de animes

--- server/anime.py
+++ server/anime.py
@@ -1,19 +1,12 @@
 import pypyodbc as odbc
 import pandas as pd
 import json
-
-server = 'azureanimedbsrvlss.database.windows.net'
-database= 'animedb'
-connection_string='Driver={ODBC Driver 18 for SQL Server};Server=azureanimedbsrvlss.database.windows.net,1433;Database=animedb;Uid=animAIDB;Pwd=@Simao0312;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-
-
-sql = '''select * from [anime-dataset-2023-filtered]
-         where anime_id = 1'''
+import dbconnection as dbconn
 
 
 def retrieve_from_database(sql_operation):
     try:
-        conn = odbc.connect(connection_string)
+        conn = odbc.connect(dbconn.connection_string)
         cursor = conn.cursor()
         cursor.execute(sql_operation)
         
--- server/app.py
+++ server/app.py
@@ -10,16 +10,27 @@ cors = CORS(app, origins='*')
 def home():
     return "<h1>Welcome to server home page</h1>"
 
-@app.route("/api", methods=['GET'])
-def testing():
-    return "🎆 CONGRATS! 🎆"
 
 @app.route("/api/get_all_animes", methods=['GET'])
 def get_all_animes():
-    sql = '''SELECT * FROM [anime-dataset-2023-filtered]'''
+    sql = '''SELECT * FROM [dbo].[anime-dataset-2023-filtered]'''
     json_data = retrieve_from_database(sql)
     print(json_data)  # For debugging purposes
     
+    try:
+        # Parse the JSON string to a Python object
+        data = json.loads(json_data)
+        return jsonify(data)
+    except json.JSONDecodeError:
+        # In case of JSON parsing error, return an error response
+        return jsonify({"error": "Failed to parse JSON data"}), 500
+    
+
+@app.route("/api/get_num_animes/<int:num_animes>", methods=['GET'])
+def get_num_animes(num_animes):
+    sql = f'''SELECT TOP ({num_animes}) * FROM [dbo].[anime-dataset-2023-filtered] ORDER BY NEWID()'''
+    json_data = retrieve_from_database(sql)
+    print(json_data)  # For debugging purposes
     try:
         # Parse the JSON string to a Python object
         data = json.loads(json_data)
